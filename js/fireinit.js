4721ee85d91c1712ef3ad2eb8c906af222c38299
mandoo1027 <40375307+mandoo1027@users.noreply.github.com>
2018-07-02 23:29:58 +0900

    Add files via upload

---
+++ js/fireinit.js
@@ -0,0 +1,10 @@
+var config = {
+	apiKey: "AIzaSyDZKVmtLazu-gC9pK_Tj1YuutXL-vC2KYw",
+	authDomain: "culturalheritage-36e7c.firebaseapp.com",
+	databaseURL: "https://culturalheritage-36e7c.firebaseio.com",
+	projectId: "culturalheritage-36e7c",
+	storageBucket: "culturalheritage-36e7c.appspot.com",
+	messagingSenderId: "541675841758"
+  };
+firebase.initializeApp(config);
+	   
